6b6025b3f6e614f439b612011d895e508b516bfe
parik panchal <parikspanchal@icloud.com>
2018-12-21 18:18:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizbuzz(end_num)
+while $num <= end_num
+if $num % 5 == 0 && $num % 3 == 0
+  puts "FizzBuzz"
+elsif $num % 3 == 0
+  puts "Fizz"
+elsif $num % 5 == 0
+  puts"Buzz"
+elsif $num % 5!= 0 && $num % 3 != 0
+  puts "nil"
+end
+$num += 1
+end
+end
