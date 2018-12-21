35b6c75b1d5f382365338635332716c4659825f2
parik panchal <parikspanchal@icloud.com>
2018-12-21 18:33:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizbuzz(num)
+def fizzbuzz(num)
 if num % 5 == 0 && num % 3 == 0
   "FizzBuzz"
 elsif num % 3 == 0
