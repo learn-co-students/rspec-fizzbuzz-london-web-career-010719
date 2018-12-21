562deff48f988b4965a992605e1c1df9742f1647
parik panchal <parikspanchal@icloud.com>
2018-12-21 18:20:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,16 @@
 # See README.md for instructions on how to do this
 
 def fizbuzz(num)
-if $num % 5 == 0 && $num % 3 == 0
+if num % 5 == 0 && num % 3 == 0
   puts "FizzBuzz"
-elsif $num % 3 == 0
+elsif num % 3 == 0
   puts "Fizz"
-elsif $num % 5 == 0
+elsif num % 5 == 0
   puts"Buzz"
-elsif $num % 5!= 0 && $num % 3 != 0
+elsif num % 5!= 0 && num % 3 != 0
   puts "nil"
 end
 end
-end
+
+
+fizbuzz(5)
