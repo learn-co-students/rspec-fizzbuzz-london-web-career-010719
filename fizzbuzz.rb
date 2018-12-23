778e0871357352a9fbec4bd528d888bfa3a5a52b
Kian Edalat <akedalat@gmail.com>
2018-12-23 12:39:55 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (int)
+  if (int % 3 == 0 && int % 5 == 0)
+    fizz_15 = "FizzBuzz"
+    return fizz_15
+  end
+  if int % 3 == 0
+    fizz_3 = "Fizz"
+    return fizz_3
+  end
+  if int % 5 == 0
+  fizz_5 =  "Buzz"
+  return fizz_5
+  end
+
+end
